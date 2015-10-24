b743d8b0d366fec8d9f0baadd04581f2e9deed55
unitycoder <support@unitycoder.com>
2015-10-25 02:06:07 +0300

    changed initial & grid values similar to original, renamed variables to start with lowerCase, public option to disable dispersion, note: problem with dispersion if left & right cells have same density (wont get pushed sideways), particle color to white

--- Assets/Scripts/ParticleSim.cs
+++ Assets/Scripts/ParticleSim.cs
@@ -11,52 +11,56 @@ public class ParticleSim : MonoBehaviour {
 
 	bool debugMode = true;
 
+	public bool enableDispersion = true;
 	public Color initialColor = new Vector4(1,1,0,2);
 
 	bool groundPlaneCollision = true;
 
-	const int particlesAmount = 128;
-	int spawnPerFrame = 1;
+	const int particlesAmount = 256;
+	int spawnRate = 3;
 
 //	float maxSpeed = 1.5f;
 //	public Transform[] obstacles;
 
-    const int Particle_Cel_Dim = 64;
+    const int particle_Cel_Dim = 32;
 	int NextParticle=0;
 	particle[] particles = new particle[particlesAmount];
-    particle_cel[,] ParticleCels = new particle_cel[Particle_Cel_Dim, Particle_Cel_Dim];
+    particle_cel[,] ParticleCels = new particle_cel[particle_Cel_Dim, particle_Cel_Dim];
 
 	public Transform sprite;
 	SpriteRenderer[] sprites;
 
-	const float GridScale = 0.05f;
-	float InvGridScale = 1f/GridScale;
+	const float gridScale = 0.25f;
+	float InvGridScale = 1f/gridScale;
 
 	float groundY = 0;
 
 
-	void Start () {
+	void Start () 
+	{
 	
 		sprites = new SpriteRenderer[particlesAmount];
 
+		// spawn particles away from the screen
 		for (int i = 0; i < sprites.Length; i++) 
 		{
 			var clone = Instantiate(sprite,Vector3.one*999,Quaternion.identity) as Transform;
+			// take references to the spawned particle
 			sprites[i] = clone.GetComponent<SpriteRenderer>();
 		}
 	}
 	
 
-	void LateUpdate () 
+	void Update () 
 	{
 
-		Vector3 GridOrigin = new Vector3(-0.5f *GridScale * Particle_Cel_Dim, 0f, 0f);
+		Vector3 gridOrigin = new Vector3(-0.5f *gridScale * particle_Cel_Dim, 0f, 0f);
 
         // reset array
        // ParticleCels = new particle_cel[Particle_Cel_Dim, Particle_Cel_Dim];      
-		for (int y = 0; y < Particle_Cel_Dim; ++y)
+		for (int y = 0; y < particle_Cel_Dim; ++y)
         {
-			for (int x = 0; x < Particle_Cel_Dim; ++x)
+			for (int x = 0; x < particle_Cel_Dim; ++x)
             {
 				ParticleCels[y, x].Density = 0;
 				//ParticleCels[y, x].VelocityTimesDensity = Vector3.zero;
@@ -90,105 +94,112 @@ public class ParticleSim : MonoBehaviour {
         }
 
         // (re)spawn particle
-		for (int ParticleSpawnIndex = 0; ParticleSpawnIndex < spawnPerFrame; ++ParticleSpawnIndex)
+		for (int particleSpawnIndex = 0; particleSpawnIndex < spawnRate; ++particleSpawnIndex)
 		{
 			NextParticle=(NextParticle+1) % particlesAmount;
 
 			// set initial values
-			particles[NextParticle].P = transform.position+new Vector3(Random.Range(-0.05f, 0.05f), 0, 0); // start pos
+			particles[NextParticle].P = transform.position+new Vector3(Random.Range(-0.05f, 0.05f), 0, 0); // start position
 //			particles[NextParticle].dP = new Vector3(Random.Range(-0.07f,0.07f),0.85f,0)*7; // start speed, global direction
-			particles[NextParticle].dP = transform.TransformDirection(new Vector3(Random.Range(-0.07f,0.07f),0.85f,0)*7); // start speed in local transform direction
+			particles[NextParticle].dP = transform.TransformDirection(new Vector3(Random.Range(-0.01f,0.01f),7f*Random.Range(0.7f,1f),0f)); // start speed in local transform direction
 			particles[NextParticle].ddP = new Vector3(0f, -9.81f, 0f); // gravity
 			particles[NextParticle].Col = initialColor;
-            particles[NextParticle].dColor = new Vector4(0, Random.Range(-0.25f, -1.25f), 0, Random.Range(-0.25f, -1f));
+			float randomVal = Random.Range(-0.25f, -1f);
+			particles[NextParticle].dColor = new Vector4(0,0,0,randomVal); // deltaColor
 
             sprites[NextParticle].transform.position = Vector3.zero;
 			sprites[NextParticle].transform.localScale = Vector3.one;
 		}
 
+
         // map particles to grid & add cell density
-        for (int ParticleIndex = 0; ParticleIndex < particles.Length; ++ParticleIndex)
+        for (int particleIndex = 0; particleIndex < particles.Length; ++particleIndex)
         {
-            Vector3 P = InvGridScale * (particles[ParticleIndex].P - GridOrigin);
+            Vector3 P = InvGridScale * (particles[particleIndex].P - gridOrigin);
 
             int X = (int)P.x;
             int Y = (int)P.y;
 
             if (X < 0) X = 0;
-            if (X > Particle_Cel_Dim-1) X = Particle_Cel_Dim-1;
+            if (X > particle_Cel_Dim-1) X = particle_Cel_Dim-1;
             if (Y < 0) Y = 0;
-            if (Y > Particle_Cel_Dim - 1) Y = Particle_Cel_Dim - 1;
+            if (Y > particle_Cel_Dim - 1) Y = particle_Cel_Dim - 1;
 
-			float Density = particles[ParticleIndex].Col.w;
+			float density = particles[particleIndex].Col.w;
 
-            ParticleCels[Y, X].Density += Density;
-            ParticleCels[Y, X].VelocityTimesDensity += Density * particles[ParticleIndex].dP;
+            ParticleCels[Y, X].Density += density;
+            ParticleCels[Y, X].VelocityTimesDensity += density*particles[particleIndex].dP;
         }
 
 
 		if (debugMode)
 		{
-			// grid preview
-			for (int y = 0; y < Particle_Cel_Dim; ++y)
+			// draw preview grid
+			for (int y = 0; y < particle_Cel_Dim; ++y)
 	        {
-	            for (int x = 0; x < Particle_Cel_Dim; ++x)
+	            for (int x = 0; x < particle_Cel_Dim; ++x)
 	            {
 
-	                float Alpha = Clamp01(0.1f*ParticleCels[y, x].Density);
-	                Color cellColor = new Color(Alpha,Alpha,Alpha,1);
+	                float alpha = Clamp01(0.1f*ParticleCels[y, x].Density);
+	                Color cellColor = new Color(alpha,alpha,alpha,1);
 
-	                Vector3 gridPos = GridScale*new Vector3(x, y, 0f) + GridOrigin;
+	                Vector3 gridPos = gridScale*new Vector3(x, y, 0f) + gridOrigin;
 
-					Debug.DrawLine(gridPos,gridPos+new Vector3(1,0,0)*GridScale,cellColor);
-					Debug.DrawLine(gridPos, gridPos + new Vector3(0, 1, 0)*GridScale, cellColor);
+					Debug.DrawLine(gridPos,gridPos+new Vector3(1,0,0)*gridScale,cellColor);
+					Debug.DrawLine(gridPos, gridPos + new Vector3(0, 1, 0)*gridScale, cellColor);
 	            }
 	        }
 		}
 
 
         // simulate
-		for (int ParticleIndex = 0; ParticleIndex < particles.Length; ++ParticleIndex)
+		for (int particleIndex = 0; particleIndex < particles.Length; ++particleIndex)
 		{
 			// snap to grid
-			Vector3 P = InvGridScale * (particles[ParticleIndex].P - GridOrigin);
-
+			Vector3 P = InvGridScale * (particles[particleIndex].P - gridOrigin);
 			int X = (int)P.x;
 			int Y = (int)P.y;
 
+			// clamp to edges
 			if (X < 1) X = 1;
-			if (X > Particle_Cel_Dim-2) X = Particle_Cel_Dim-2;
+			if (X > particle_Cel_Dim-2) X = particle_Cel_Dim-2;
 			if (Y < 1) Y = 1;
-			if (Y > Particle_Cel_Dim - 2) Y = Particle_Cel_Dim - 2;
+			if (Y > particle_Cel_Dim - 2) Y = particle_Cel_Dim - 2;
 
-			// neighbour cells
-			var CelCenter = ParticleCels[Y, X].Density;
-			var CelLeft = ParticleCels[Y, X-1].Density;
-			var CelRight = ParticleCels[Y, X+1].Density;
-			var CelDown = ParticleCels[Y-1, X].Density;
-			var CelUp = ParticleCels[Y+1, X].Density;
+			// get neighbour cell values
+			float celCenterDensity = ParticleCels[Y, X].Density;
+			float celLeftDensity = ParticleCels[Y, X-1].Density;
+			float celRightDensity = ParticleCels[Y, X+1].Density;
+			float celDownDensity = ParticleCels[Y-1, X].Density;
+			float celUpDensity = ParticleCels[Y+1, X].Density;
 
-			Vector3 Dispersion = Vector3.zero;
-			float Dc = 1f;
-			Dispersion += Dc*(CelCenter-CelLeft)*new Vector3(-1f,0f,0f);
-			Dispersion += Dc*(CelCenter-CelRight)*new Vector3(1f,0f,0f);
-			Dispersion += Dc*(CelCenter-CelDown)*new Vector3(0f,-1f,0f);
-			Dispersion += Dc*(CelCenter-CelUp)*new Vector3(0f,1f,0f);
+			// calculate direction from cell density
+			Vector3 dispersion = Vector3.zero;
+			float dispersionCoeffifient = 1f; // multiplier of pushing force
 
-			Vector3 ddP = particles[ParticleIndex].ddP+Dispersion;
+			dispersion += dispersionCoeffifient*(celCenterDensity-celLeftDensity)*new Vector3(-1f,0f,0f);
+			dispersion += dispersionCoeffifient*(celCenterDensity-celRightDensity)*new Vector3(1f,0f,0f);
+			dispersion += dispersionCoeffifient*(celCenterDensity-celDownDensity)*new Vector3(0f,-1f,0f);
+			dispersion += dispersionCoeffifient*(celCenterDensity-celUpDensity)*new Vector3(0f,1f,0f);
+
+			Vector3 ddP = particles[particleIndex].ddP+(enableDispersion?dispersion:Vector3.zero);
+
+			// debug test
+//			if (particleIndex<1) Debug.Log(particles[particleIndex].dP);
 
 			// calculate movement & color
-			particles[ParticleIndex].P += (0.5f*Mathf.Sqrt(Time.deltaTime)*Time.deltaTime*ddP + Time.deltaTime*particles[ParticleIndex].dP);
-			particles[ParticleIndex].dP += Time.deltaTime*ddP;
-			particles[ParticleIndex].Col += Time.deltaTime*particles[ParticleIndex].dColor;
+			particles[particleIndex].P += (0.5f*Mathf.Sqrt(Time.deltaTime)*Time.deltaTime*ddP + Time.deltaTime*particles[particleIndex].dP);
+			particles[particleIndex].dP += Time.deltaTime*ddP;
+			particles[particleIndex].Col += Time.deltaTime*particles[particleIndex].dColor;
 
             // ground collision & bounce
-			if (groundPlaneCollision && particles[ParticleIndex].P.y<groundY)
+			if (groundPlaneCollision && particles[particleIndex].P.y<groundY)
             {
-                float CoefficientOfRestitution = 0.3f;
-				float CoefficientOfFriction = 0.7f;
-				particles[ParticleIndex].P.y = -particles[ParticleIndex].P.y;
-				particles[ParticleIndex].dP.y = -CoefficientOfRestitution*particles[ParticleIndex].dP.y;
-				particles[ParticleIndex].dP.x = CoefficientOfFriction*particles[ParticleIndex].dP.x;
+                float coefficientOfRestitution = 0.3f;
+				float coefficientOfFriction = 0.7f;
+				particles[particleIndex].P.y = -particles[particleIndex].P.y;
+				particles[particleIndex].dP.y = -coefficientOfRestitution*particles[particleIndex].dP.y;
+				particles[particleIndex].dP.x = coefficientOfFriction*particles[particleIndex].dP.x;
 			}
 
 			/*
@@ -199,22 +210,22 @@ public class ParticleSim : MonoBehaviour {
 			}*/
 
 			// clamp colors
-			particles[ParticleIndex].Col.x = Clamp01(particles[ParticleIndex].Col.x);
-			particles[ParticleIndex].Col.y = Clamp01(particles[ParticleIndex].Col.y);
-			particles[ParticleIndex].Col.z = Clamp01(particles[ParticleIndex].Col.z);
-			particles[ParticleIndex].Col.w = Clamp01(particles[ParticleIndex].Col.w);
+			particles[particleIndex].Col.x = Clamp01(particles[particleIndex].Col.x);
+			particles[particleIndex].Col.y = Clamp01(particles[particleIndex].Col.y);
+			particles[particleIndex].Col.z = Clamp01(particles[particleIndex].Col.z);
+			particles[particleIndex].Col.w = Clamp01(particles[particleIndex].Col.w);
 
             // fix particle sudden spawn by making it transparent first, not working smoothly
-            if (particles[ParticleIndex].Col.w>0.9f)
+            if (particles[particleIndex].Col.w>0.9f)
             {
-                particles[ParticleIndex].Col.w = 0.9f*Clamp01MapToRange(1f,(particles[ParticleIndex].Col.w),0.9f);
+                particles[particleIndex].Col.w = 0.9f*Clamp01MapToRange(1f,(particles[particleIndex].Col.w),0.9f);
             }
 
 			// set position, color, scale
-			sprites[ParticleIndex].transform.position = particles[ParticleIndex].P;
-			sprites[ParticleIndex].color = (Color)particles[ParticleIndex].Col;
-            var s = particles[ParticleIndex].Col.w;
-			sprites[ParticleIndex].transform.localScale = new Vector3(s, s, s)*0.75f;
+			sprites[particleIndex].transform.position = particles[particleIndex].P;
+			sprites[particleIndex].color = (Color)particles[particleIndex].Col;
+            var scaler = particles[particleIndex].Col.w;
+			sprites[particleIndex].transform.localScale = new Vector3(scaler, scaler, scaler)*0.75f; // scale test
 		}
 	}
 
